bf42bd84093189972b8ba8da3c0841bf896f73a0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-07-14 19:19:47 +0200

    scaffolding

---
+++ mapc2020/__init__.py
@@ -0,0 +1,9 @@
+"""A client for the 2020/21 edition of the Multi-Agent Programming Contest."""
+
+from __future__ import annotations
+
+__version__ = "0.1.0"  # Remember to update setup.py
+
+
+class AsyncAgent:
+    pass
